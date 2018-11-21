28b29ed98351246e08f2649ce14a1a36e392b177
rkdgusdn9 <rkdgusdn9@gmail.com>
2018-11-21 16:58:43 +1300

    second commit

--- GymRat/Controllers/GymController.cs
+++ GymRat/Controllers/GymController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using GymRat.Models;
+using MemeBank.Helpers;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.Extensions.Configuration;
 
 namespace GymRat.Controllers
 {
@@ -15,9 +20,12 @@ namespace GymRat.Controllers
     {
         private readonly GymRatContext _context;
 
-        public GymController(GymRatContext context)
+        private IConfiguration _configuration;
+
+        public GymController(GymRatContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/Gym
@@ -128,6 +136,107 @@ namespace GymRat.Controllers
             var returned = await gym.ToListAsync();
 
             return returned;
+        }
+
+        [HttpPost, Route("upload")]
+        public async Task<IActionResult> UploadFile([FromForm] GymImageItem gym)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+            try
+            {
+                using (var stream = gym.Image.OpenReadStream())
+                {
+                    var cloudBlock = await UploadToBlob(gym.Image.FileName, null, stream);
+                    //// Retrieve the filename of the file you have uploaded
+                    //var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+                    if (string.IsNullOrEmpty(cloudBlock.StorageUri.ToString()))
+                    {
+                        return BadRequest("An error has occured while uploading your file. Please try again.");
+                    }
+
+                    GymItem gymItem = new GymItem();
+                    gymItem.Name = gym.Name;
+                    gymItem.BigPart = gym.BigPart;
+
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
+                    gymItem.Height = image.Height.ToString();
+                    gymItem.Width = image.Width.ToString();
+                    gymItem.Url = cloudBlock.SnapshotQualifiedUri.AbsoluteUri;
+                    gymItem.Uploaded = DateTime.Now.ToString();
+
+                    _context.GymItem.Add(gymItem);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"File: {gym.Name} has successfully uploaded");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+
+        }
+
+        private async Task<CloudBlockBlob> UploadToBlob(string filename, byte[] imageBuffer = null, System.IO.Stream stream = null)
+        {
+
+            var accountName = _configuration["AzureBlob:name"];
+            var accountKey = _configuration["AzureBlob:key"]; ;
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference("images");
+
+            string storageConnectionString = _configuration["AzureBlob:connectionString"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Generate a new filename for every new blob
+                    var fileName = Guid.NewGuid().ToString();
+                    fileName += GetFileExtention(filename);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = imagesContainer.GetBlockBlobReference(fileName);
+
+                    if (stream != null)
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return new CloudBlockBlob(new Uri(""));
+                    }
+
+                    return cloudBlockBlob;
+                }
+                catch (StorageException ex)
+                {
+                    return new CloudBlockBlob(new Uri(""));
+                }
+            }
+            else
+            {
+                return new CloudBlockBlob(new Uri(""));
+            }
+
+        }
+
+        private string GetFileExtention(string fileName)
+        {
+            if (!fileName.Contains("."))
+                return ""; //no extension
+            else
+            {
+                var extentionList = fileName.Split('.');
+                return "." + extentionList.Last(); //assumes last item is the extension 
+            }
         }
 
         private bool GymItemExists(int id)
---
+++ GymRat/Models/GymImageItem.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymRat.Models
+{
+    public class GymImageItem
+    {
+        public string Name { get; set; }
+        public string BigPart { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
