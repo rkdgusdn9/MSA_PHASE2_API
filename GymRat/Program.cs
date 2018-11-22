b4b923d8eb254927fb7b6d321f2902bc263e28df
rkdgusdn9 <rkdgusdn9@gmail.com>
2018-11-22 23:29:35 +1300

    App insights commit

--- GymRat/Program.cs
+++ GymRat/Program.cs
@@ -41,6 +41,7 @@ namespace GymRat
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
